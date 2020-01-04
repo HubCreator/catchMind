0ffddaf6b0432e094711476273efd8925458c045
HubCreator <chocomilk8604@gmail.com>
2020-01-04 16:52:57 +0900

    Connect Socket.io

---
+++ src/server.js
@@ -0,0 +1,31 @@
+import { join } from "path";
+import express from "express";
+import socketIO from "socket.io";
+import logger from "morgan";
+
+const PORT = 5000;
+const app = express();
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
+app.use(express.static(join(__dirname, "static")));
+app.get("/", (req, res) => res.render("home"));
+
+const handleListening = () => {
+  console.log(`Server running: http://localhost:${PORT}`);
+  // console.log(__dirname);
+  // console.log(join(__dirname, "views"));
+};
+
+const server = app.listen(PORT, handleListening);
+
+const io = socketIO.listen(server);
+
+let sockets = [];
+
+// io.on("connection", socket => console.log(socket));
+io.on("connection", socket => {
+  sockets.push(socket.id);
+});
+
+setInterval(() => console.log(sockets), 1000);
