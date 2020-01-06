dffd20844e63e54df1b5aa95aedb01b1e602c67d
HubCreator <chocomilk8604@gmail.com>
2020-01-06 10:48:33 +0900

    More about the Gulp and Using Browserify

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,10 @@
 import gulp from "gulp"; // as like webpack
-import sass from "gulp-sass"; // can compile Sass into CSS
-import autoprefixer from "gulp-autoprefixer"; // make our code compatable in various websites automatically
+import sass from "gulp-sass"; // can compile SCSS into CSS
+import autoprefixer from "gulp-autoprefixer"; // make our code compatible in various websites automatically
 import minifyCSS from "gulp-csso"; // make our CSS code minified
+import del from "del";
+import browserify from "gulp-browserify";
+import babelify from "babelify";
 
 sass.compiler = require("node-sass");
 
@@ -10,11 +13,18 @@ const paths = {
     src: "assets/scss/styles.scss",
     dest: "src/static/styles",
     watch: "assets/scss/**/*.scss"
+  },
+  js: {
+    src: "assets/js/main.js",
+    dest: "src/static/js",
+    watch: "assets/js/**/*.js"
   }
 };
 
-function styles() {
-  return gulp
+const clean = () => del(["src/static"]);
+
+const styles = () =>
+  gulp
     .src(paths.styles.src) // from
     .pipe(sass()) // interpret or compile scss into CSS
     .pipe(
@@ -25,12 +35,26 @@ function styles() {
     )
     .pipe(minifyCSS())
     .pipe(gulp.dest(paths.styles.dest)); // to
-}
 
-function watchFiles() {
+const js = () =>
+  gulp
+    .src(paths.js.src)
+    .pipe(
+      browserify({
+        transform: [
+          babelify.configure({
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          })
+        ]
+      })
+    )
+    .pipe(gulp.dest(paths.js.dest));
+
+const watchFiles = () => {
   gulp.watch(paths.styles.watch, styles);
-}
+  gulp.watch(paths.js.watch, js);
+};
 
-const dev = gulp.series([styles, watchFiles]);
+const dev = gulp.series(clean, styles, js, watchFiles);
 
 export default dev;
