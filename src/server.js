aff858f11a1333fb19cbcddce09bcf95dadc69b0
HubCreator <chocomilk8604@gmail.com>
2020-01-07 10:34:35 +0900

    Making login function

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
-import logger from "morgan";
+import morgan from "morgan";
+import socketController from "./socketController";
 
 const PORT = 5000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
-app.use(logger("dev"));
+app.use(morgan("dev"));
 app.use(express.static(join(__dirname, "static")));
 app.get("/", (req, res) => res.render("home"));
 
@@ -21,16 +22,4 @@ const server = app.listen(PORT, handleListening);
 
 const io = socketIO.listen(server);
 
-io.on("connection", socket => {
-  // socket.emit : Send messages from server to client(browser) directly
-  // socket.broadcast.emit : Send message all clients except the connected client
-  socket.on("newMessage", ({ message }) => {
-    socket.broadcast.emit("messageNotif", {
-      message,
-      nickname: socket.nickname || "Anonymous"
-    });
-  });
-  socket.on("setNickName", ({ nickname }) => {
-    socket.nickname = nickname; // User can make varibales under socket object
-  });
-});
+io.on("connection", socket => socketController(socket));
