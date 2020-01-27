36babdfd12c4a6157dbcf426e55469b5d393f147
HubCreator <chocomilk8604@gmail.com>
2020-01-28 00:01:08 +0900

    Game Part 2

--- assets/js/players.js
+++ assets/js/players.js
@@ -1,1 +1,14 @@
-export const handlePlayerUpdate = ({ sockets }) => console.log(sockets);
+const playerInfo = document.getElementById("jsParticipants");
+
+const showPlayerInfo = sockets => {
+  sockets.forEach(aSocket => {
+    //const span = document.createElement("span");
+    playerInfo.innerText = aSocket.nickname + " ";
+    // playerInfo.appendChild(span);
+  });
+};
+
+export const handlePlayerUpdate = ({ sockets }) => {
+  playerInfo.innerText = "";
+  showPlayerInfo(sockets);
+};
